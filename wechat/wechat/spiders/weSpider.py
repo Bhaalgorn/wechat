e06c5c03796051f40fcddf4f4c306c28e91e92c5
Bhaalgorn <megayueh@hotmail.com>
2016-05-16 14:08:28 +0800

    1408

--- wechat/wechat/spiders/weSpider.py
+++ wechat/wechat/spiders/weSpider.py
@@ -3,7 +3,7 @@ from scrapy.selector import Selector
 
 class weSpider(scrapy.spiders.Spider):
     name = "weSpider"
-    start_url = ["http://mp.weixin.qq.com/s?timestamp=1463127018&src=3&ver=1&signature=mNPkSOqWJD-E-l8UBwRa3IiBjKbc8SW2tfDhHxlQGfJRVLRbmocGVQAX2gBvjSLlXtQQPxcpzWwhjIB5paKAQxLS3sYKXhYdklLK*8mV5tCmt00JiLMJZaXmCB6yiXayRsu3OqIxY6NFV12c9Y-kEDirOWkG0XXjZTFg8*R1MEQ="]
+    start_urls = ["http://mp.weixin.qq.com/s?timestamp=1463127018&src=3&ver=1&signature=mNPkSOqWJD-E-l8UBwRa3IiBjKbc8SW2tfDhHxlQGfJRVLRbmocGVQAX2gBvjSLlXtQQPxcpzWwhjIB5paKAQxLS3sYKXhYdklLK*8mV5tCmt00JiLMJZaXmCB6yiXayRsu3OqIxY6NFV12c9Y-kEDirOWkG0XXjZTFg8*R1MEQ="]
     def parse(self,response):
         filename = 'excited'
         with open(filename,'wb') as f:
