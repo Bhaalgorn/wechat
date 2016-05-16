3e7c093f3e5dbeeec1c52b89f4b918b5f36cc5ab
Bhaalgorn <megayueh@hotmail.com>
2016-05-16 16:44:33 +0800

    1644

--- wechat/wechat/items.py
+++ wechat/wechat/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class WechatItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    body = scrapy.Field()
     pass
--- wechat/wechat/spiders/weSpider.py
+++ wechat/wechat/spiders/weSpider.py
@@ -1,10 +1,19 @@
+#coding=utf-8
 import scrapy
 from scrapy.selector import Selector
+from scrapy.http import Request
+from wechat.items import WechatItem
 
 class weSpider(scrapy.spiders.Spider):
     name = "weSpider"
     start_urls = ["http://mp.weixin.qq.com/s?timestamp=1463127018&src=3&ver=1&signature=mNPkSOqWJD-E-l8UBwRa3IiBjKbc8SW2tfDhHxlQGfJRVLRbmocGVQAX2gBvjSLlXtQQPxcpzWwhjIB5paKAQxLS3sYKXhYdklLK*8mV5tCmt00JiLMJZaXmCB6yiXayRsu3OqIxY6NFV12c9Y-kEDirOWkG0XXjZTFg8*R1MEQ="]
     def parse(self,response):
-        filename = 'excited'
-        with open(filename,'wb') as f:
-            f.write(response.body)
+        sel =Selector(response)
+        item = WechatItem()
+        a = sel.xpath("//title")
+        a = a.xpath("text()").extract()
+        a = str(a)
+        print a
+        f = open('native.txt','w')
+        f.write(a)
+        f.close()
